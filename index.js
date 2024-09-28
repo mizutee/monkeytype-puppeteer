9870a7fc244c2d74bce00bbf89a55880f77a46a6
chrisansen <c.wanadi@citis.kompasgramedia.com>
2024-09-28 15:57:25 +0700

    Adding validation to prevent stuck on cookies modal

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const puppeteer = require('puppeteer');
     await page.setViewport({ width: screenSize.width, height: screenSize.height });
     await page.goto('https://www.monkeytype.com/')
     const modal = await page.waitForSelector('div.modal div.main div.buttons')
-    await modal.click('button. active acceptAll')
+    if (modal) {
+        await modal.click('button. active acceptAll')
+    }
     const app = await page.waitForSelector('#app main .page.pageTest.full-width.content-grid.active #typingTest #wordsWrapper #words')
-    let count = 0
     while (true) {
         try {
             let activeWords = await app.waitForSelector('.word.active', { timeout: '3000' })
@@ -23,8 +24,6 @@ const puppeteer = require('puppeteer');
             });
             const inputElement = await page.waitForSelector('#wordsInput');
             await inputElement.type(`${letters.join('')} `);
-            count += 1
-            console.log(`Typed Words: ${count}`)
         } catch (error) {
             if (error.name === 'TimeoutError') {
                 break
